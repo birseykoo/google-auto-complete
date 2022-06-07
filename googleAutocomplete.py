db056db13b18eb78bbdef22e4281aa2d1606475c
volkan <volkancaliskan14@gmail.com>
2022-06-07 23:44:29 +0300

    Update

--- googleAutocomplete.py
+++ googleAutocomplete.py
@@ -1,31 +1,19 @@
-from sqlite3 import Row
-from token import NEWLINE
 import requests
-import pandas as pd
-import string
 import xml.etree.ElementTree as ET
 
 
-list_a = ["aranacak kelime","aranacak kelime"]
+list_a = ["python"]  # aranacak kelimeyi giriniz
 list_b = [" ", "a", "b", "c", "ç", "d", "e", "f", "g", "ğ", "h", "i", "ı", "j", "k", "l",
-          "m", "n", "o" "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "y", "z", "x", "q", "w"]
+          "m", "n", "o" "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "y", "z", "x", "q", "w"]  # Harf veya Kelime Ekleyebilirsiniz
 list_c = [f"{i} {j}" for i in list_a for j in list_b]
 
 for x in list_c:
-    apiurl = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=tr&q="+x
+    apiurl = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=tr&q=" + \
+        x  # Google Suggest API
     r = requests.get(apiurl)
     tree = ET.fromstring(r.text)
-    
+
     for child in tree.iter('suggestion'):
         data = child.attrib['data'], "\n"
-        dosya = open("keywords_data.xls","a+")
+        dosya = open("keywords_data.xls", "a+")
         dosya.writelines(data)
-        
-
-
-
-
-
-
-
-
